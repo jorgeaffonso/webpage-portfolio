59f155ae2525e47205bb8357f2f84c40dd612f76
Jorge <jorgemail.affonso@gmail.com>
2022-12-28 15:29:41 -0300

    Day 22- Add app description, image and links

--- main.py
+++ main.py
@@ -16,14 +16,22 @@ with col2:
 content2 = """ Below you can find some of the apps I have built in Python. Feel free to contact me!"""
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+        #st.write("[Source code](https://pythonhow.com)")
+
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
