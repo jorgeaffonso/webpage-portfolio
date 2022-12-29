709b044da605453ca4dcd789efae2f5e938ddaed
Jorge <jorgemail.affonso@gmail.com>
2022-12-29 10:51:26 -0300

    Day 23- Send email

--- home.py
+++ home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images\B&W.jpg", width=340)
+    st.image("images\B&W.jpg", width=240)
 
 with col2:
     st.title("Jorge Luis Affonso")
--- pages/contact_us.py
+++ pages/contact_us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Contact Us")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Enter your email:")
+    user_message = st.text_area("Your message:")
+    button = st.form_submit_button("Submit")
+    if button:
+        print("Button was pressed")
+
