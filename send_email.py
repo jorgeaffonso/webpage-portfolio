e54c47c4d39fae49c884dc396830f0342f5bd10f
Jorge <jorgemail.affonso@gmail.com>
2022-12-30 12:46:25 -0300

    Day 23 - Get the password from de user environment variable

--- send_email.py
+++ send_email.py
@@ -1,18 +1,17 @@
 import smtplib, ssl
+import os
+
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "jorgemail.affonso@gmail.com"
-    password = "ffdmwzibpnvrzuph"
+    #password = "..."
+    password = os.getenv("PASSWORD")   # password for gmail application - saved in user environment variable
     receiver = "jorgemail.affonso@gmail.com"
     my_context = ssl.create_default_context()
-#    message = """\
-#Subject: Hi!
-#How are you doing ?
-#Bye!
-#"""
+
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
